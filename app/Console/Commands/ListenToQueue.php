b9df8efe15eaa13e2a017e08c16069d9a153d167
Yuliana Alich <yalich@infraspeak.com>
2020-10-23 11:19:10 +0100

    redis:queue command ++

--- app/Console/Commands/ListenToQueue.php
+++ app/Console/Commands/ListenToQueue.php
@@ -4,6 +4,7 @@ namespace App\Console\Commands;
 
 use App\Models\User;
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Redis;
 
 class ListenToQueue extends Command
 {
@@ -38,6 +39,8 @@ class ListenToQueue extends Command
      */
     public function handle()
     {
-        
+        Redis::subscribe(['COMPOSER_FILE'], function($message) {
+            dump($message);
+        });
     }
 }
