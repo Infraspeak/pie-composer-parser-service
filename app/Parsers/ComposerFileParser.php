def8eb3331c7f198f5eee50d1538fcfee00957cd
jsa <jsa@infraspeak.com>
2022-10-18 12:57:28 +0100

    Fix bug

--- app/Parsers/ComposerFileParser.php
+++ app/Parsers/ComposerFileParser.php
@@ -3,21 +3,23 @@
 namespace App\Parsers;
 
 use App\Repositories\PackagistRepository;
+use http\Exception\InvalidArgumentException;
+use PHPUnit\Exception;
 
 class ComposerFileParser
 {
-    public static function parse(array $composerFile)
+    public static function parse(array $composerFile): array
     {
-        $requirements = array_merge($composerFile['require'], $composerFile['require-dev']);
+            $requirements = array_merge($composerFile['require'] ?? [], $composerFile['require-dev'] ?? []);
 
-        $result = [];
-        foreach ($requirements as $name => $version) {
-            $result[] = [
-                'name' => $name,
-                'version' => $version,
-                'url' => PackagistRepository::getUrl($name),
-            ];
-        }
-        return $result;
+            $result = [];
+            foreach ($requirements as $name => $version) {
+                $result[] = [
+                    'name' => $name,
+                    'version' => $version,
+                    'url' => PackagistRepository::getUrl($name),
+                ];
+            }
+            return $result;
     }
 }
